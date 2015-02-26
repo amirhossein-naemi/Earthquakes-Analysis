11991e03cbc3be1d337aa0bca0fa25c67319bc10
Amirhossein <amirhnaimi@gmail.com>
2015-02-25 18:34:45 -0600

    v1
    start

---
+++ amir.cpp
@@ -0,0 +1,17 @@
+
+/*
+ * Earthquakes-Analysis
+ * By Amirhossein Naemi
+ * Febuary, 2015
+ * GIT: https://github.com/amirhossein-naemi/Fibonacci
+ * This program reads in one integer from a amir.in file and returns the
+ * Fibonacci sequence
+ */
+
+using namespace std;
+
+int main() {
+
+    return 0;
+    
+}
